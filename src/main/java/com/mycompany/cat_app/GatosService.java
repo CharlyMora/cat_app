ab65aa5067156529c85adb638f1c42d787e6f506
CharlyMora <morandres.carlos@gmail.com>
2021-03-09 16:28:39 -0500

    first verification of menu

---
+++ src/main/java/com/mycompany/cat_app/GatosService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cat_app;
+
+/**
+ *
+ * @author Andres
+ */
+public class GatosService {
+    public static void VerGatos(){
+        
+        
+    }
+}
--- src/main/java/com/mycompany/cat_app/Inicio.java
+++ src/main/java/com/mycompany/cat_app/Inicio.java
@@ -20,8 +20,22 @@ public class Inicio {
         };
         
         do{
-            String opcion = (String) JOptionPane.showInputDialog(null,"Gatitos java","menu principal", JOptionPane.INFORMATION_MESSAGE,null, botones, botones[0]);
+            String opcion = (String) JOptionPane.showInputDialog(null,"Gatitos java","menu principal", JOptionPane.INFORMATION_MESSAGE,
+                    null, botones, botones[0]);
             
+            for(int i =0; i<botones.length; i++){
+                if(opcion.equals(botones[i])){
+                    opcion_menu=i;
+                }
+            }
+            
+            switch(opcion_menu){
+                case 0:
+                    GatosService.VerGatos();
+                    break;
+                default:
+                    break;
+            }
         }while(opcion_menu != 1);
         
         
